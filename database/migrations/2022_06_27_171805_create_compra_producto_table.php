f2c12f0733602207e4db107e960773a802b05972
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2022-06-28 10:21:39 -0600

    Nuevas tablas

---
+++ database/migrations/2022_06_27_171805_create_compra_producto_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compra_producto', function (Blueprint $table) {
+            $table->id();
+            $table->integer('producto_id');
+            $table->integer('compra_id');
+            $table->integer('cantidad');
+            $table->float('precio_compra');
+            $table->float('precio_venta');
+            $table->float('total_compra');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compra_producto');
+    }
+};
